c74fd5101925cae9f93f7cd79e22b6aa6972357e
Bobby Martin <robert.martin.619@gmail.com>
2018-05-04 13:20:58 -0700

    Add head links component

---
+++ src/app/header-links/header-links.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-links',
+  templateUrl: './header-links.component.html',
+  styleUrls: ['./header-links.component.scss']
+})
+export class HeaderLinksComponent {
+
+  constructor() { }
+
+}
