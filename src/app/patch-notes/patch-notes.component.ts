842b44bff11b30f86a9851269025b7eacd05a1fa
Bobby Martin <robert.martin.619@gmail.com>
2018-05-09 19:21:31 -0700

    Add service to patch notes and move other stuff to patch note details component

--- src/app/patch-notes/patch-notes.component.ts
+++ src/app/patch-notes/patch-notes.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
 import { PatchNote } from './../models/patch-note.model';
+import { PatchNotesService } from './../services/patch-notes.service';
+
 @Component({
   selector: 'app-patch-notes',
   templateUrl: './patch-notes.component.html',
-  styleUrls: ['./patch-notes.component.scss']
+  styleUrls: ['./patch-notes.component.scss'],
+  providers: [PatchNotesService]
 })
+
 export class PatchNotesComponent {
-  @Input() childPatchNoteList: PatchNote[];
-  constructor() { }
+  patchNotes: FirebaseListObservable<any[]>;
+  constructor(private router: Router, private patchNotesService: PatchNotesService) { }
+
+  goToPatchNotePage(clickedPatchNote) {
+    this.router.navigate(["patch-notes", clickedPatchNote.$key])
+  }
+  ngOnInit() {
+    this.patchNotes = this.patchNotesService.getPatchNotes();
+  }
 
 }
