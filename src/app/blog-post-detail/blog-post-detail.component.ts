80a92ee0e7c4ce1c95f10a7e68cead4f57e4a67f
Bobby Martin <robert.martin.619@gmail.com>
2018-05-11 15:40:23 -0700

    Add blog post detail componenet

---
+++ src/app/blog-post-detail/blog-post-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-detail',
+  templateUrl: './blog-post-detail.component.html',
+  styleUrls: ['./blog-post-detail.component.scss']
+})
+export class BlogPostDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
