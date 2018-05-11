9cf2a094aafbb3a1245abb554450a9af1a4f741a
Bobby Martin <robert.martin.619@gmail.com>
2018-05-11 13:04:49 -0700

    Add twitter widget code

--- src/app/social-media/social-media.component.ts
+++ src/app/social-media/social-media.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-media.component.scss']
 })
 export class SocialMediaComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    // This is from twitters own embed documentation
+    !function(d,s,id){
+      var js: any,
+          fjs=d.getElementsByTagName(s)[0],
+          p='https';
+      if(!d.getElementById(id)){
+          js=d.createElement(s);
+          js.id=id;
+          js.src=p+"://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js,fjs);
+      }
+    }(document,"script","twitter-wjs");
   }
 
 }
